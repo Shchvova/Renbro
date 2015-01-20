c8ae4c058ff3bb48b41f612459461440c9b9b6c3
Vlad Svoka <shchvova@gmail.com>
2015-01-20 15:21:52 -0500

    line implemented

--- main.cpp
+++ main.cpp
@@ -11,43 +11,74 @@ template <typename T> int sgn(T val) {
 }
 
 
-void line(Vec2i a, Vec2i b, TGAImage & image, const TGAColor & col) {
+void line_obvious(Vec2i a, Vec2i b, TGAImage & image, const TGAColor & col) {
     const auto d = b-a;
     const int dx = abs(d.x);
     const int dy = abs(d.y);
-    if(dy > dx) {
 
+    if(dx > dy) {
+        if(a.x > b.x)
+            swap(a,b);
+
+        int s;
+        if(dx>0)
+            s = sgn(b.y-a.y);
+        else
+            s = 0;
+
+        for(int x=0; x<= dx; x++) {
+            image.set(a.x + x, a.y + x*s*dy/dx, col);
+        }
+    }else if (dy>0)
+    {
+
+        if(a.y > b.y)
+            swap(a,b);
+
+        int s;
+        if(dy>0)
+            s = sgn(b.x-a.x);
+        else
+            s = 0;
+
+        for(int y=0; y<= dy; y++) {
+            image.set(a.x + y*s*dx/dy, a.y+y, col);
+        }
+    }
+    else
+    {
+        for(int x=0; x<= dx; x++) {
+            image.set(a.x + x, a.y, col);
+        }
     }
 }
 
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-    bool steep = false;
-    if (std::abs(x0-x1)<std::abs(y0-y1)) {
-        std::swap(x0, y0);
-        std::swap(x1, y1);
-        steep = true;
+void line(Vec2i a, Vec2i b, TGAImage &image, const TGAColor &col) {
+    bool swap = false;
+    if (abs(a.x-b.x)<abs(a.y-b.y)) {
+        a.swap();
+        b.swap();
+        swap = true;
     }
-    if (x0>x1) {
-        std::swap(x0, x1);
-        std::swap(y0, y1);
+    if (a.x>b.x) {
+        std::swap(a, b);
     }
-    int dx = x1-x0;
-    int dy = y1-y0;
-    int derror2 = std::abs(dy)*2;
-    int error2 = 0;
-    int y = y0;
-    for (int x=x0; x<=x1; x++) {
-        if (steep) {
-            image.set(y, x, color);
+    Vec2i d = b-a;
+    int dError = abs(d.y)*2;
+    int error = 0;
+    int y = a.y;
+    for (int x=a.x; x<=b.x; x++) {
+        if (swap) {
+            image.set(y, x, col);
         } else {
-            image.set(x, y, color);
+            image.set(x, y, col);
         }
-        error2 += derror2;
+        error += dError;
 
-        if (error2 > dx) {
-            y += (y1>y0?1:-1);
-            error2 -= dx*2;
+        if (error > d.x) {
+            y += (b.y>a.y?1:-1);
+            error -= d.x*2;
         }
     }
 }
@@ -58,8 +89,6 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 int main() {
     int width = 800, height = 800;
     TGAImage image(width, height, TGAImage::RGB);
-    TGAImage image2(width, height, TGAImage::RGB);
-
 
     Model model(RES("head.obj"));
 
@@ -68,27 +97,20 @@ int main() {
         for (int j=0; j<3; j++) {
             Vec3f v0 = model.vert(face[j]);
             Vec3f v1 = model.vert(face[(j+1)%3]);
-            int x0 = (v0.x+1.0)*width/2.0;
-            int y0 = (v0.y+1.0)*height/2.0;
-            int x1 = (v1.x+1.0)*width/2.0;
-            int y1 = (v1.y+1.0)*height/2.0;
+            int x0 = int((v0.x+1.0)*width*0.5);
+            int y0 = int((v0.y+1.0)*height*0.5);
+            int x1 = int((v1.x+1.0)*width*0.5);
+            int y1 = int((v1.y+1.0)*height*0.5);
             line(Vec2i(x0, y0), Vec2i(x1, y1), image, white);
-            line(x0, y0, x1, y1, image2, white);
+            //line(x0, y0, x1, y1, image2, white);
         }
     }
 
 
-//    line(Vec2i(100, 5), Vec2i(300, 700), image, white);
-//    line(100, 5, 300, 700, image2, white);
-//
-//    line(Vec2i(300, 700),Vec2i(100, 5),  image, white);
-//    line(300, 700, 100, 5,  image2, white);
 
     image.flip_vertically();
-    image2.flip_vertically();
 
     image.write_tga_file(OUT("output.tga"));
-    image2.write_tga_file(OUT("output2.tga"));
 
     return 0;
 }
