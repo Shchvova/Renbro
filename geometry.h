9ec12c8c862d003f048666d267624cf4f3e3574c
Vlad Svoka <shchvova@gmail.com>
2015-01-20 19:18:03 -0500

    fixing warning. swap shouldn't return anything

--- geometry.h
+++ geometry.h
@@ -16,7 +16,7 @@ template <class t> struct Vec2 {
     inline Vec2<t> operator +(const Vec2<t> &V) const { return Vec2<t>(u+V.u, v+V.v); }
     inline Vec2<t> operator -(const Vec2<t> &V) const { return Vec2<t>(u-V.u, v-V.v); }
     inline Vec2<t> operator *(float f)          const { return Vec2<t>(u*f, v*f); }
-    inline Vec2<t> swap() {std::swap(x,y);}
+    inline void swap() {std::swap(x,y);}
     template <class > friend std::ostream& operator<<(std::ostream& s, Vec2<t>& v);
 };
 
